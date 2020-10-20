895e21b9bd03305224f0fb37b1aea0e740affa9b
Yosua Marchel <yosua.marchel@gmail.com>
2020-10-20 11:18:59 +0700

    First commit for the framework

---
+++ CustomRoundedFramework/CustomRoundedFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomRoundedFramework.h
+//  CustomRoundedFramework
+//
+//  Created by Yosua Marchel on 20/10/20.
+//  Copyright © 2020 Yosua Marchel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedFramework.
+FOUNDATION_EXPORT double CustomRoundedFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedFramework/PublicHeader.h>
+
+
